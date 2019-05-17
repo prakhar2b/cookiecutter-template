94ec5822957c7d14f98013def36ca2c4b07b3a0b
Prakhar Pratyush <er.prakhar2b@gmail.com>
2019-05-17 13:27:39 +0530

    update conftest

--- {{cookiecutter.project_name}}/conftest.py
+++ {{cookiecutter.project_name}}/conftest.py
@@ -1,1 +1,9 @@
+from selenium import webdriver
+import pytest
 
+@pytest.fixture
+def test_setup():
+  global driver
+  driver = webdriver.Chrome(executable_path = 'chromedriver')
+  driver.implicitly_wait({{cookiecutter.global_step_wait}})
+ 
